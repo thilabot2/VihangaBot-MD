10ef9480db4739a5063ab184752f9415f9bb92d7
thilabot2 <112172576+thilabot2@users.noreply.github.com>
2022-08-26 22:13:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
-global.ig = 'https://github.com/vihangabot'
-global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
-global.sc = 'https://github.com/vihangabot'
-global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.owner = ['94742261385']
+global.premium = ['94742261385']
+global.ownernomer = '94742261385'
+global.ownername = '៚Thilina Dilshan⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.botname = 'Thila'
+global.footer = '@thila'
+global.cap = '*Genarated By Thila*'
+global.ig = 't'
+global.region = ''
+global.sc = ''
+global.myweb = 'https://www.youtube.com/channel/UC_aOLJqreXrsP5qciJ4xAog'
+global.packname = '@Thila Sticker'
+global.author = '៚Thila⁸⁵⁴¹⃟✦⃝̮̮〽️'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
